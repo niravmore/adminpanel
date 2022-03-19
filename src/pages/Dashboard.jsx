d3df162abf9974e6f86a640b8820bf4d7cacc326
niravmore <niravmore1990@gmail.com>
2022-03-19 20:27:41 +0530

    add statuscard and chart

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
 
+import Chart from 'react-apexcharts'
+
 import Statuscard from '../components/status_card/Statuscard'
 
 import statusCards from '../assets/JsonData/statuscarddata.json'
+
+const chartOptions = {
+	series: [{
+		name: 'Online Customers',
+		data: [40, 70, 20, 90, 36, 80, 30, 91, 60, 50, 40, 55]
+	}, {
+		name: 'Store Customers',
+		data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10, 25, 45]
+	}],
+	options: {
+		color: ['#6ab04c', '#2980b9'],
+		chart: {
+			background: 'transparent'
+		},
+		dataLabels: {
+			enabled: false
+		},
+		stroke: {
+			curve: 'smooth'
+		},
+		xaxis: {
+			categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+		},
+		legend: {
+			position: 'top'
+		},
+		grid: {
+			show: false
+		}
+	}
+}
 
 const Dashboard = () => {
 	return (
@@ -14,8 +47,6 @@ const Dashboard = () => {
 						{
 							statusCards.map((item, index) => (
 								<div className='col-6'>
-									{/* */}
-									{item.title}
 									<Statuscard
 										icon={item.icon}
 										count={item.count}
@@ -24,6 +55,17 @@ const Dashboard = () => {
 								</div>
 							))
 						}
+					</div>
+				</div>
+				<div className='col-6'>
+					<div className='card full-height'>
+						{/*charts*/}
+						<Chart
+							options={chartOptions.options}
+							series={chartOptions.series}
+							type='line'
+							height='100%'
+						/>
 					</div>
 				</div>
 			</div>
